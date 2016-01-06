ef87060b52c6553021d6f36e648ed0358d4efafc
Collin Melton <cmelton@stanford.edu>
2016-01-06 13:59:53 -0800

    configuring logging?

--- DDServerApp/FlaskApp/WSGI.py
+++ DDServerApp/FlaskApp/WSGI.py
@@ -1,6 +1,11 @@
 from WebApp import app
 
-if __name__ == "__main__":
-    from werkzeug.debug import DebuggedApplication
-    app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     from werkzeug.debug import DebuggedApplication
+#     app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+#     app.run(debug=True)
+    
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig()
+    app.run(debug=False) # debug must be off on production for security reasons
