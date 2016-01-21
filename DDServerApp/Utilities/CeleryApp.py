6c8c5d678fd66f4fb791de2d40714a237085bf52
Collin Melton <cmelton@stanford.edu>
2016-01-21 10:50:18 -0800

    test module for celery

---
+++ DDServerApp/Utilities/CeleryApp.py
@@ -0,0 +1,13 @@
+'''
+Created on Jan 21, 2016
+
+@author: cmelton
+'''
+
+from celery import Celery
+
+app = Celery('CeleryApp', broker='amqp://guest@localhost//')
+
+@app.task
+def add(x, y):
+    return x + y
